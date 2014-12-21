3c4349f74a7ebbb613f49571921309b56f54fee5
Smirnov Mikhail <sm_m_a@mail.ru>
2014-12-21 20:36:15 +0300

    corrected small remarks.

--- thirdSemester/HW 1/BinaryTree/BinaryTree/BinaryTree.cs
+++ thirdSemester/HW 1/BinaryTree/BinaryTree/BinaryTree.cs
@@ -9,12 +9,22 @@
     /// <typeparam name="T"> Type of element. </typeparam>
     public class BinaryTree<T> : IEnumerable<T> where T : IComparable
     {
+        private class Node
+        {
+            public Node(T value)
+            {
+                this.Value = value;
+            }
+
+            public T Value { get; set; }
+            public Node LeftChild { get; set; }
+            public Node RightChild { get; set; }
+        }
+
         private Node root;
 
         public BinaryTree()
-        {
-            this.root = null;
-        }
+        {}
 
         /// <summary>
         /// Method for add element in tree.
@@ -27,7 +37,31 @@
             else  if (this.root == null)
                 this.root = new Node(value);
             else
-                this.root.Add(value);
+                Add(value, this.root);
+        }
+
+        private void Add(T value, Node root)
+        {
+            if (root.Value.CompareTo(value) > 0)
+            {
+                if (root.LeftChild == null)
+                {
+                    var newNode = new Node(value);
+                    root.LeftChild = newNode;
+                }
+                else
+                    Add(value, root.LeftChild);
+            }
+            else
+            {
+                if (root.RightChild == null)
+                {
+                    var newNode = new Node(value);
+                    root.RightChild = newNode;
+                }
+                else
+                    Add(value, root.RightChild);
+            }
         }
 
         /// <summary>
@@ -38,61 +72,28 @@
         public bool Exist(T value)
         {
             if (this.root != null)
-                return this.root.Exist(value);
+                return this.Exist(value, root);
 
             return false;
         }
 
-        /// <summary>
-        /// Method for delete element in tree.
-        /// </summary>
-        /// <param name="value"> This element. </param>
-        public void Delete(T value)
+        private bool Exist(T value, Node root)
         {
-            if (this.root == null || !this.Exist(value))
-                return;
-            else if (this.root.Value.CompareTo(value) != 0)
+            if (root.Value.CompareTo(value) == 0)
+                return true;
+            else if (root.Value.CompareTo(value) > 0)
             {
-                if (this.root.Value.CompareTo(value) < 0)
-                    this.root.LeftChild.Delete(value, this.root);
+                if (root.LeftChild == null)
+                    return false;
                 else
-                    this.root.RightChild.Delete(value, this.root);
+                    return Exist(value, root.LeftChild);
             }
             else
             {
-                if (this.root.RightChild == null && this.root.LeftChild == null)
-                    this.root = null;
-                else if (this.root.LeftChild == null)
-                    this.root = this.root.RightChild;
-                else if (this.root.RightChild == null)
-                    this.root = this.root.LeftChild;
+                if (root.RightChild == null)
+                    return false;
                 else
-                {
-                    if (this.root.LeftChild.RightChild == null)
-                    {
-                        this.root.LeftChild.RightChild = this.root.RightChild;
-                        this.root = this.root.LeftChild;
-                    }
-                    else
-                    {
-                        var currentNode = this.root.LeftChild.RightChild;
-                        var tempNode = new Node(default(T));
-
-                        if (currentNode.RightChild == null)
-                            this.root.LeftChild.RightChild = null;
-                        else
-                        {
-                            while (currentNode.RightChild.RightChild != null)
-                                currentNode = currentNode.RightChild;
-                            tempNode = currentNode.RightChild;
-                            currentNode.RightChild = null;
-                        }
-
-                        tempNode.RightChild = this.root.RightChild;
-                        tempNode.LeftChild = this.root.LeftChild;
-                        this.root = tempNode;
-                    }
-                }
+                    return Exist(value, root.RightChild);
             }
         }
 
@@ -102,11 +103,18 @@
         public void Print()
         {
             if (this.root == null)
-            {
                 Console.WriteLine("Tree is empty!");
-            }
             else
-                this.root.Print();
+                this.Print();
+        }
+
+        private void Print(Node root)
+        {
+            Console.WriteLine(root.Value);
+            if (root.LeftChild != null)
+                this.Print(root.LeftChild);
+            if (root.RightChild != null)
+                this.Print(root.RightChild);
         }
 
         /// <summary>
@@ -119,224 +127,59 @@
         }
 
         /// <summary>
-        /// Tree element.
+        /// Method for delete element in tree.
         /// </summary>
-        private class Node
+        /// <param name="value"> This element. </param>
+        /// <returns></returns>
+        public void Delete(T value)
         {
-            public Node(T value)
-            {
-                this.Value = value;
-                this.LeftChild = null;
-                this.RightChild = null;
-            }
-
-            /// <summary>
-            /// Method for add element in tree.
-            /// </summary>
-            /// <param name="value"> This element. </param>
-            public void Add(T value)
-            {
-                if (this.Value.CompareTo(value) < 0)
-                {
-                    if (this.LeftChild == null)
-                    {
-                        var newNode = new Node(value);
-                        this.LeftChild = newNode;
-                    }
-                    else
-                        this.LeftChild.Add(value);
-                }
-                else
-                {
-                    if (this.RightChild == null)
-                    {
-                        var newNode = new Node(value);
-                        this.RightChild = newNode;
-                    }
-                    else
-                        this.RightChild.Add(value);
-                }
-            }
-
-            /// <summary>
-            /// Method for search element in tree.
-            /// </summary>
-            /// <param name="value"> This element. </param>
-            /// <returns></returns>
-            public bool Exist(T value)
-            {
-                if (this.Value.CompareTo(value) == 0)
-                    return true;
-                else if (this.Value.CompareTo(value) < 0)
-                {
-                    if (this.LeftChild == null)
-                        return false;
-                    else
-                        return this.LeftChild.Exist(value);
-                }
-                else
-                {
-                    if (this.RightChild == null)
-                        return false;
-                    else
-                        return this.RightChild.Exist(value);
-                }
-            }
-
-            /// <summary>
-            /// Method for delete element in tree.
-            /// </summary>
-            /// <param name="value"> This element. </param>
-            /// <param name="previous"> Previous element. </param>
-            public void Delete(T value, Node previous)
-            {   
-                if (this.Value.CompareTo(value) == 0)
-                {
-                    if (this.LeftChild == null && this.RightChild == null)
-                    {
-                        if (previous.LeftChild == null)
-                            previous.RightChild = null;
-                        else if (previous.RightChild == null || previous.LeftChild.Value.CompareTo(value) == 0)
-                            previous.LeftChild = null;
-                        else
-                            previous.RightChild = null;
-                    }
-                    else if (this.LeftChild == null)
-                    {
-                        if (previous.LeftChild == null)
-                            previous.RightChild = this.RightChild;
-                        else if (previous.RightChild == null || previous.LeftChild.Value.CompareTo(value) == 0)
-                            previous.LeftChild = this.RightChild;
-                        else
-                            previous.RightChild = this.RightChild;
-                    }
-                    else if (this.RightChild == null)
-                    {
-                        if (previous.LeftChild == null)
-                            previous.RightChild = this.LeftChild;
-                        else if (previous.RightChild == null || previous.LeftChild.Value.CompareTo(value) == 0)
-                            previous.LeftChild = this.LeftChild;
-                        else
-                            previous.RightChild = this.LeftChild;
-                    }
-                    else
-                    {
-                        if (previous.LeftChild == null)
-                        {
-                            if (this.RightChild.LeftChild == null)
-                            {
-                                previous.RightChild = this.RightChild;
-                                this.RightChild.LeftChild = this.LeftChild;
-                            }
-                            else
-                            {
-                                var currentNode = this.RightChild.LeftChild;
-                                var tempNode = new Node(default(T));
-
-                                if (currentNode.LeftChild == null)
-                                    this.RightChild.LeftChild = null;
-                                else
-                                {
-                                    while (currentNode.LeftChild.LeftChild != null)
-                                        currentNode = currentNode.LeftChild;
-                                    tempNode = currentNode.LeftChild;
-                                    currentNode.LeftChild = null;
-                                }
-
-                                previous.RightChild = tempNode;
-                                tempNode.LeftChild = this.LeftChild;
-                                tempNode.RightChild = this.RightChild;
-                            }
-                        }
-                        else if (previous.RightChild == null || previous.LeftChild.Value.CompareTo(value) == 0)
-                        {
-                            if (this.LeftChild.RightChild == null)
-                            {
-                                previous.LeftChild = this.LeftChild;
-                                this.LeftChild.RightChild = this.RightChild;
-                            }
-                            else
-                            {
-                                var currentNode = this.LeftChild.RightChild;
-                                var tempNode = new Node(default(T));
-
-                                if (currentNode.RightChild == null)
-                                    this.LeftChild.RightChild = null;
-                                else
-                                {
-                                    while (currentNode.RightChild.RightChild != null)
-                                        currentNode = currentNode.RightChild;
-                                    tempNode = currentNode.RightChild;
-                                    currentNode.RightChild = null;
-                                }
-
-                                previous.LeftChild = tempNode;
-                                tempNode.LeftChild = this.LeftChild;
-                                tempNode.RightChild = this.RightChild;
-                            }
-                        }
-                        else
-                        {
-                            if (this.RightChild.LeftChild == null)
-                            {
-                                previous.RightChild = this.RightChild;
-                                this.RightChild.LeftChild = this.LeftChild;
-                            }
-                            else
-                            {
-                                var currentNode = this.RightChild.LeftChild;
-                                var tempNode = new Node(default(T));
-
-                                if (currentNode.LeftChild == null)
-                                    this.RightChild.LeftChild = null;
-                                else
-                                {
-                                    while (currentNode.LeftChild.LeftChild != null)
-                                        currentNode = currentNode.LeftChild;
-                                    tempNode = currentNode.LeftChild;
-                                    currentNode.LeftChild = null;
-                                }
-
-                                previous.RightChild = tempNode;
-                                tempNode.LeftChild = this.LeftChild;
-                                tempNode.RightChild = this.RightChild;
-                            }
-                        }
-                    }
-                }
-                else if (this.Value.CompareTo(value) < 0)
-                {
-                    if (this.LeftChild == null)
-                        return;
-                    else
-                        this.LeftChild.Delete(value, this.LeftChild);
-                }
-                else
-                {
-                    if (this.RightChild == null) 
-                        return;
-                    else
-                        this.RightChild.Delete(value, this.RightChild);
-                }
-            }
-
-            /// <summary>
-            /// Method for write tree.
-            /// </summary>
-            public void Print()
-            {
-                Console.WriteLine(this.Value);
-                if (this.LeftChild != null)
-                    this.LeftChild.Print();
-                if (this.RightChild != null)
-                    this.RightChild.Print();   
-            }
-
-            public T Value { get; set; }
-            public Node LeftChild { get; set; }
-            public Node RightChild { get; set; }
+            root = Delete(value, root);
         }
 
+        private Node Delete(T value, Node root)
+        {
+            if (root == null)
+                return null;
+            if (value.CompareTo(root.Value) < 0)
+                root.LeftChild = Delete(value, root.LeftChild);
+            else if (value.CompareTo(root.Value) > 0)
+                root.RightChild = Delete(value, root.RightChild);
+            else
+            {
+                if (root.LeftChild == null && root.RightChild == null)
+                    return null;
+                else if ((root.LeftChild != null && root.RightChild == null) || (root.LeftChild == null && root.RightChild != null))
+                    return (root.LeftChild != null) ? root.LeftChild : root.RightChild;
+                else
+                {
+                    if (root.RightChild.LeftChild == null)
+                    {
+                        Node tmp = root.LeftChild;
+                        root = root.RightChild;
+                        root.LeftChild = tmp;
+                        return root;
+                    }
+                    else
+                    {
+                        Node tmp = root.LeftChild;
+                        root = root.RightChild;
+                        Node tmp2 = root;
+                        while (tmp2.LeftChild.LeftChild != null)
+                        {
+                            tmp2 = tmp2.LeftChild;
+                        }
+                        tmp2.LeftChild.LeftChild = tmp;
+                        return root;
+                    }
+                }
+            }
+            return root;
+        }
+
+        /// <summary>
+        /// Tree enumerator.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
         private class NodeIterator : IEnumerator<T>
         {
             private Node currentElement;
@@ -350,21 +193,26 @@
                 stack.Push(tree.root);
             }
 
+            /// <summary>
+            /// Return current value.
+            /// </summary>
             public T Current
             {
                 get { return currentElement.Value; }
             }
 
             void IDisposable.Dispose()
-            {
-
-            }
+            {}
 
             object System.Collections.IEnumerator.Current
             {
                 get { return Current; }
             }
 
+            /// <summary>
+            /// Move enumenator for next element and check end of tree.
+            /// </summary>
+            /// <returns></returns>
             public bool MoveNext()
             {
                 if (stack.Count == 0)
@@ -380,6 +228,9 @@
                 }
             }
 
+            /// <summary>
+            /// Sets initial value.
+            /// </summary>
             void System.Collections.IEnumerator.Reset()
             {
                 stack.Clear();
@@ -387,11 +238,19 @@
             }
         }
 
+        /// <summary>
+        /// Return tree enumerator for this class.
+        /// </summary>
+        /// <returns></returns>
         public IEnumerator<T> GetEnumerator()
         {
             return new NodeIterator(this);
         }
 
+        /// <summary>
+        /// GetEnumerator
+        /// </summary>
+        /// <returns></returns>
         System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
--- thirdSemester/HW 1/BinaryTree/BinaryTree/Program.cs
+++ thirdSemester/HW 1/BinaryTree/BinaryTree/Program.cs
@@ -6,21 +6,7 @@
     {
         static void Main()
         {
-            BinaryTree<Int32> tree = new BinaryTree<int>();
-            
-            tree.Add(5);
-            tree.Delete(5);
-            //tree.Add(10);
-            //tree.Add(15);
-            //tree.Add(20);
-            //tree.Add(13);
-           // tree.Add(12);
-            if (tree.Exist(5))
-            {
-                Console.WriteLine("!!!");
-            }
 
-            tree.Print();
         }
     }
 }
