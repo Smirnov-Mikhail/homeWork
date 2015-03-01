477104d4b79ba8fa9e162a2a46f34a697d04cf58
Smirnov Mikhail <sm_m_a@mail.ru>
2015-03-01 17:24:23 +0300

    Переделал без for

--- fourthSemester/HW 2/ListOfPowersOfTwo/ListOfPowersOfTwo/ListOfPowersOfTwo.fs
+++ fourthSemester/HW 2/ListOfPowersOfTwo/ListOfPowersOfTwo/ListOfPowersOfTwo.fs
@@ -1,4 +1,9 @@
-﻿let createList2 startPow =
-    let list = [startPow]
-    let collectList = List.collect (fun x -> [for i in 0..4 -> x * (2.0 ** float(i))]) list
-    collectList
+﻿let formListOfPowersOfTwo startPower =
+    let list = []
+    let rec recFormList list currentPow iter =
+        if iter <> 5 then
+            let list = list @ [currentPow]
+            recFormList list (currentPow * 2) (iter + 1)
+        else
+            list
+    recFormList list startPower 0
