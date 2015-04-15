f663ce81f48ccf3057ad0d37748de3ac9fbf48de
Smirnov Mikhail <sm_m_a@mail.ru>
2015-04-15 23:30:59 +0300

    Task 1, hw 4

---
+++ fourthSemester/HW 4/Task 1 (hooks)/Task 1 (hooks)/hooks.fs
@@ -0,0 +1,19 @@
+﻿let oppositeClose a b =
+    (a = '(' && b = ')') || (a = '{' && b = '}') || (a = '[' && b = ']')
+
+let test (str: string) = 
+    let stack = []
+    let rec recTest stack (str: string) =
+        if (List.length stack <> 0 && String.length str <> 0 && oppositeClose (List.head stack) str.[0]) then
+            recTest (List.tail stack) str.[1..str.Length - 1]
+        else if (str.Length <> 0) then
+            recTest (str.[0]::stack) str.[1..str.Length - 1]    
+        else if ((List.length stack) = 0) then
+            true
+        else
+            false           
+    recTest stack str
+            
+printfn "%A" (test "(({}[]){})")
+
+  
