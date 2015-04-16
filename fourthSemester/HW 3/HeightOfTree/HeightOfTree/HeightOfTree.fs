d4e77d8d975da84d1dc4fa51afb5cdea1666e207
Smirnov Mikhail <sm_m_a@mail.ru>
2015-04-16 19:37:43 +0300

    some correction

--- fourthSemester/HW 3/HeightOfTree/HeightOfTree/HeightOfTree.fs
+++ fourthSemester/HW 3/HeightOfTree/HeightOfTree/HeightOfTree.fs
@@ -1,12 +1,12 @@
 ﻿type 'a Tree =
-    | EmptyTree of 'a
+    | Leaf of 'a
     | Tree of 'a * 'a Tree * 'a Tree
 
 let rec heightOfTree tree =
     match tree with
-    | EmptyTree _ -> 1
+    | Leaf _ -> 1
     | Tree(_, l, r) ->
         1 + max (heightOfTree l) (heightOfTree r)
 
-let tree = Tree(10, Tree(2, EmptyTree(1), EmptyTree(5)), EmptyTree(40));
+let tree = Tree(10, Tree(2, Leaf(1), Leaf(5)), Leaf(40));
 printfn "%A" (heightOfTree tree)
