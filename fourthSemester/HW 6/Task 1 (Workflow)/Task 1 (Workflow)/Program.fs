8e5e273caab9a6dffb0c2cfe455b2e36b9008b1e
Smirnov Mikhail <sm_m_a@mail.ru>
2015-04-19 16:28:41 +0300

    HW 6

---
+++ fourthSemester/HW 6/Task 1 (Workflow)/Task 1 (Workflow)/Program.fs
@@ -0,0 +1,8 @@
+﻿type RoundingBuilder(digit : int) =
+
+    member this.Bind (x : float, rest : float -> float) =
+        System.Math.Round(rest x, digit)
+
+    member this.Return x = x
+
+let rounding digit = RoundingBuilder digit
