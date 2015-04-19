7f090ddb5dc472b660d8800426a4b77f5de84161
Smirnov Mikhail <sm_m_a@mail.ru>
2015-04-19 16:28:45 +0300

    HW 7

---
+++ fourthSemester/HW 7/Task 1 (sum of one million units)/HW 7 (sum of one million units)/SumOfUnits.fs
@@ -0,0 +1,18 @@
+﻿let sum items =
+    let mutable result = 0
+    for i in items do
+        result <- result + i  
+    result 
+
+let sumOfArray sum (items: array<int>) =
+    let tasks =         
+        seq {            
+            for i in 0..Array.length items / 10000 - 1 -> async  {                
+                return (sum items.[i * 10000..(i + 1) * 10000 - 1])  
+            }                    
+        }
+    sum (Async.RunSynchronously (Async.Parallel tasks))
+
+let array = [| for i in 1 .. 1000000 -> 1 |]
+// Результат вроде не странный...
+printfn "%A" (sumOfArray sum array)
