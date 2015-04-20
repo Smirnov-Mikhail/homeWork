85ed86dba5a01d4adf3224707364f91ed7a22338
Smirnov Mikhail <sm_m_a@mail.ru>
2015-04-20 12:16:16 +0300

    исправлена ошибка с option type

--- fourthSemester/HW 1/Fibonacci/Fibonacci/Fibonacci.fs
+++ fourthSemester/HW 1/Fibonacci/Fibonacci/Fibonacci.fs
@@ -1,5 +1,14 @@
-﻿let rec fibonacci n =
+﻿let sumOption (x1 : int option) (x2 : int option) =
+    match x1 with
+    | Some x -> x + 
+                match x2 with
+                | Some x -> x
+                | None -> 0
+    | None -> 0
+
+let rec fibonacci n =
     match n with
     | n when n < 0 -> None
-    | 1 | 2 -> Some(1)
-    | n -> fibonacci(n - 1) + fibonacci (n - 2)
+    | 0 -> Some(0)
+    | 1 -> Some(1)
+    | n -> Some(sumOption (fibonacci(n - 1)) (fibonacci (n - 2)))
