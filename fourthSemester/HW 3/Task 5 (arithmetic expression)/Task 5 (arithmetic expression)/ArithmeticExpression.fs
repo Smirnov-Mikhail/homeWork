03638fc076ceec1e638e1ee996328b5d2d4ab463
Smirnov Mikhail <sm_m_a@mail.ru>
2015-04-23 16:59:05 +0300

    Hw 3, task 5

---
+++ fourthSemester/HW 3/Task 5 (arithmetic expression)/Task 5 (arithmetic expression)/ArithmeticExpression.fs
@@ -0,0 +1,37 @@
+﻿type Tree<'a, 'b> =
+    | Leaf of 'b
+    | Tree of 'a * Tree<'a, 'b> * Tree<'a, 'b>
+
+exception IncorrectOperation of string
+exception DivisionByZero of string
+
+let rec calculateTree tree =
+    match tree with
+    | Tree(operation, left, right) ->
+        match operation with
+        | '+' -> calculateTree left + calculateTree right
+        | '-' -> calculateTree left - calculateTree right
+        | '*' -> calculateTree left * calculateTree right
+        | '/' ->
+                if(calculateTree right = 0) then
+                    raise (DivisionByZero "Division by zero")
+                else
+                    calculateTree left / calculateTree right
+        | _ -> raise (IncorrectOperation "Incorrect operation!")
+    | Leaf value -> value
+
+let tree1 = Tree('*', Tree('+', Leaf(1), Leaf(2)), Leaf(3))
+
+try
+    printfn "%A" (calculateTree tree1)
+with
+    | IncorrectOperation(str) -> printfn "%s" str
+    | DivisionByZero(str) -> printfn "%s" str
+
+let tree2 = Tree('/', Tree('+', Leaf(1), Leaf(2)), Leaf(0))
+
+try
+    printfn "%A" (calculateTree tree2)
+with
+    | IncorrectOperation(str) -> printfn "%s" str
+    | DivisionByZero(str) -> printfn "%s" str
