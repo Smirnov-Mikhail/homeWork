9b9441cf62aa95cf3d56ba1af2b5d41391b35fa5
Smirnov Mikhail <sm_m_a@mail.ru>
2015-05-15 14:14:13 +0300

    исправление номера 1 из третьего дз

--- fourthSemester/HW 3/FindPosition/FindPosition/FindPosition.fs
+++ fourthSemester/HW 3/FindPosition/FindPosition/FindPosition.fs
@@ -1,13 +1,13 @@
 ﻿let findPosition list =
-    let rec recFindPosition list max iter =       
+    let rec recFindPosition list max iter result =       
         match list with
-        | head :: tail when (list.Length < 3) -> Some(iter)
-        | head :: tail -> 
-            if (max < head + tail.Tail.Head) then
-                recFindPosition tail (head + tail.Tail.Head) (iter + 1)
+        | head :: tail when tail <> [] -> 
+            if (max < head + tail.Head) then
+                recFindPosition tail (head + tail.Head) (iter + 1) iter
             else
-                recFindPosition tail max (iter + 1)
-        | [] -> None
-    recFindPosition list 0 0
+                recFindPosition tail max (iter + 1) result
+        | _ -> result
+        
+    recFindPosition list 0 0 -1
 
-printf "%A" (findPosition [ 1; 5; 6; 2 ])
+printf "%A" (findPosition [1; 5; 6; 2; 11])
