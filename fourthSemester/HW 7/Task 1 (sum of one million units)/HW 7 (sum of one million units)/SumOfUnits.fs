68129dbfd668c5c42036a2390298eb80dd6bcc0f
Smirnov Mikhail <sm_m_a@mail.ru>
2015-05-26 18:50:35 +0300

    fixed solution
    теперь ответ странный

--- fourthSemester/HW 7/Task 1 (sum of one million units)/HW 7 (sum of one million units)/SumOfUnits.fs
+++ fourthSemester/HW 7/Task 1 (sum of one million units)/HW 7 (sum of one million units)/SumOfUnits.fs
@@ -1,18 +1,25 @@
-﻿let sum items =
-    let mutable result = 0
-    for i in items do
-        result <- result + i  
-    result 
+﻿open System.ComponentModel
 
-let sumOfArray sum (items: array<int>) =
-    let tasks =         
-        seq {            
-            for i in 0..Array.length items / 10000 - 1 -> async  {                
-                return (sum items.[i * 10000..(i + 1) * 10000 - 1])  
-            }                    
-        }
-    sum (Async.RunSynchronously (Async.Parallel tasks))
+let array = Array.init 1000000 (fun x -> 1)
 
-let array = [| for i in 1 .. 1000000 -> 1 |]
-// Результат вроде не странный...
-printfn "%A" (sumOfArray sum array)
+let sum10000Elements startPos =
+    let rec sum i acc =
+        if i > startPos + 9999 then acc
+        else sum (i + 1) (acc + array.[i])
+    sum startPos 0
+
+let mutable res = 0
+
+let flags = Array.init 100 (fun x -> false)
+
+for i in 0..99 do
+    let worker = new BackgroundWorker()
+    worker.DoWork.Add(fun args -> args.Result <- box <| sum10000Elements (i * 10000)) 
+    worker.RunWorkerCompleted.Add(fun args -> 
+                                    res <- res + (args.Result :?> int)
+                                    flags.[i] <- true)
+    worker.RunWorkerAsync()
+
+while not (Array.forall(fun x -> x = true) flags) do ()
+
+printfn "%A" res
