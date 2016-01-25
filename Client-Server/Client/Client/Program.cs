f53f83f5cfb72100689ee3260dac38c1f3257623
Smirnov Mikhail <sm_m_a@mail.ru>
2016-01-25 20:15:10 +0300

    Client-Server

---
+++ Client-Server/Client/Client/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace ExampleTcpClient
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.IO;
+    using System.Net;
+    using System.Net.Sockets;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Enter the ip server:");
+                Connect(Console.ReadLine());
+            }
+        }
+
+        static void Connect(String server)
+        {
+            try
+            {
+                Int32 port = 9595;
+                TcpClient client = new TcpClient(server, port);
+                NetworkStream stream = client.GetStream();
+
+                while (true)
+                {
+                    Console.WriteLine("Enter message:");
+                    String message = Console.ReadLine();
+
+                    Byte[] buffer = System.Text.Encoding.ASCII.GetBytes(message);
+
+                    stream.Write(buffer, 0, buffer.Length);
+                    Console.WriteLine("Sent: {0}", message);
+
+                    if (message == "close")
+                        break;
+
+                    Int32 countBytes = stream.Read(buffer, 0, buffer.Length);
+                    Console.WriteLine("Response: {0}", System.Text.Encoding.ASCII.GetString(buffer));
+                }
+
+                stream.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+        }
+    }
+}
---
+++ Client-Server/Server/Server/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Server
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.IO;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Threading;
+
+    class ExampleTcpListener
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = null;
+            Int32 port = 9595;
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+            server = new TcpListener(localAddr, port);
+            server.Start();
+
+            UInt32 counter = 0;
+            while (true)
+            {
+                Console.WriteLine("Waiting for a connection...");
+
+                ThreadPool.QueueUserWorkItem(ProcessingRequest, server.AcceptTcpClient());
+                Console.WriteLine("Connection №" + (++counter).ToString());
+            }
+        }
+
+        static void ProcessingRequest(object client_obj)
+        {
+            Byte[] buffer = new Byte[256];
+
+            TcpClient client = client_obj as TcpClient;
+
+            NetworkStream stream = client.GetStream();
+
+            Int32 i;
+            while ((i = stream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                String data = System.Text.Encoding.ASCII.GetString(buffer, 0, i);
+
+                if (data == "close")
+                {
+                    Console.WriteLine("Close the connection");
+                    break;
+                }
+                else
+                {
+                    // Do something with the data.
+                    data = new string(data.Reverse().ToArray());
+
+                    Byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                    stream.Write(msg, 0, msg.Length);
+                }
+            }
+
+            client.Close();
+        }
+    }
+}
